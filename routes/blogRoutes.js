93243a55913fde23484c7e382a02b10d2107c905
khayri <khayri@khayri-VivoBook-ASUSLaptop-X421IA-M433IA>
2023-09-25 18:27:05 +0100

    lesson8 commit

--- routes/blogRoutes.js
+++ routes/blogRoutes.js
@@ -1,51 +1,12 @@
-const express=require('express')
-const router=express.Router(); 
-const Blog = require('../models/blog'); // imported from the models folder
+const express = require('express');
+const blogController = require('../controllers/blogController');
 
+const router = express.Router();
 
+router.get('/create', blogController.blog_create_get);
+router.get('/', blogController.blog_index);
+router.post('/', blogController.blog_create_post);
+router.get('/:id', blogController.blog_details);
+router.delete('/:id', blogController.blog_delete);
 
-router.get('/create', (req, res) => {
-    res.render('create', { title: 'Create a new blog' });
-  });
-  
-  router.get('/'  , (req, res) => {
-    Blog.find().sort({ createdAt: -1 }) // get all blogs from db and render them in the browser 
-      .then(result => {
-        res.render('index', { blogs: result, title: 'All blogs' });
-      })
-      .catch(err => {
-        console.log(err);
-      });
-  });
-  router.post('/', (req, res) => {
-    const blog = new Blog(req.body)
-    blog.save()
-      .then(result=>{ 
-        res.redirect('/blogs') // redirect  the user to the home page
-      }) 
-      .catch(err=>cons.log(err))
-  })
-  
-  router.get('/:id', (req, res) => {
-    const id = req.params.id;
-    Blog.findById(id)
-      .then(result => {
-        res.render('details', { blog: result, title: 'Blog Details' });
-      })
-      .catch(err => {
-        console.log(err);
-      });
-  });
-  
-  router.delete('/:id', (req, res) => {
-    const id = req.params.id;
-    
-    Blog.findByIdAndDelete(id) // delete from the database using the ID 
-      .then(result => {
-        res.json({ redirect: '/blogs' }); // send the json to the browser
-      })
-      .catch(err => {
-        console.log(err);
-      });
-  });
-  module.exports = router;
+module.exports = router;
